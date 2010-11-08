1696f3685bfa3a655e5191b69a8729117f7eb198
Ohad Levy <ohadlevy@gmail.com>
2010-11-08 12:26:11 +0200

    Updating to point to new yum repo, and include the testing yum repo

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,10 +1,17 @@
 class foreman::package {
 
   yumrepo { "foreman":
-    descr => "Foreman Repo",
-    baseurl => "http://theforeman.org/repo",
+    descr => "Foreman stable repository",
+    baseurl => "http://yum.theforeman.org/stable",
     gpgcheck => "0",
     enabled => "1"
+  }
+
+  yumrepo { 'foreman-testing':
+    enabled => '0',
+    gpgcheck => '0',
+    descr => 'Foreman testing repository',
+    baseurl => 'http://yum.theforeman.org/test'
   }
 
   package{"foreman":
