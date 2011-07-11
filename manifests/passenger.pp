78c6b9d28c1dc3566c7adb4ab4636cc23c8e93ef
Ohad Levy <ohadlevy@gmail.com>
2011-07-11 10:18:44 +0300

    fixes #1024 - adds missing comma in puppet manifests.
    provided by shannon.mitchell@fusiontechnology-llc.com

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -7,7 +7,7 @@ class foreman::passenger {
   file{"foreman_vhost":
     path => $lsbdistid ? {
       default => "/etc/httpd/conf.d/foreman.conf",
-      "Ubuntu" => "/etc/apache2/conf.d/foreman.conf"
+      "Ubuntu" => "/etc/apache2/conf.d/foreman.conf",
       "Debian" => "/etc/apache2/conf.d/foreman.conf"
     },
     content => template("foreman/foreman-vhost.conf.erb"),
