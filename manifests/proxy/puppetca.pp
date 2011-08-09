e3391e9374af234f4d0adac1ea28eb2f5a2c51c9
Ohad Levy <ohadlevy@gmail.com>
2011-08-09 16:00:03 +0300

    fixed missing permissions

--- manifests/proxy/puppetca.pp
+++ manifests/proxy/puppetca.pp
@@ -4,6 +4,7 @@ class foreman::proxy::puppetca {
     owner => $foreman_proxy_user, 
     group => "puppet",
     mode  => 644,
+    ensure => present,
     require => Package["foreman-proxy"],
   }
 
@@ -14,6 +15,11 @@ class foreman::proxy::puppetca {
     "foreman_proxy_user_does_not_require_tty_in_sudo":
       file    => "/etc/sudoers",
       line    => "Defaults:${foreman_proxy_user} !requiretty";
+  }
+
+  user {$foreman_proxy_user:
+    groups => ["puppet"],
+    notify => Service["foreman-proxy"],
   }
 
 }
