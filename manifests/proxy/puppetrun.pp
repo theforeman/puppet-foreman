6fb7b8366cd310f8339dd424d9531a98cca9aa7d
Ohad Levy <ohadlevy@gmail.com>
2011-09-21 13:47:16 +0300

    split out smart proxy to its own puppet module

--- manifests/proxy/puppetrun.pp
+++
@@ -1,9 +0,0 @@
-class foreman::proxy::puppetrun {
-  
-  myline {
-    "allow_foreman_proxy_to_execute_puppetrun":
-      file => "/etc/sudoers",
-      line => "${foreman_proxy_user} ALL = NOPASSWD: /usr/bin/puppetrun"
-  }
-
-}
