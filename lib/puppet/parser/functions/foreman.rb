bf87d952234ca7951b36da63fc616ce5b1f9d6ab
W. Andrew Loe III <andrew@andrewloe.com>
2011-12-20 11:20:19 -0800

    Use Net::HTTP directly to avoid gem dependencies.

--- lib/puppet/parser/functions/foreman.rb
+++ lib/puppet/parser/functions/foreman.rb
@@ -16,29 +16,35 @@
 # which in this case will return a hash
 # {"xyz":{"manufacturer":"LENOVO"}}
 
-require "rubygems"
-require "rest_client"
+require "net/http"
+require "net/https"
 require "uri"
 require "timeout"
 
-
 module Puppet::Parser::Functions
   newfunction(:foreman, :type => :rvalue) do |args|
-    #URL to query
-    foreman_url  = "http://foreman"
-    foreman_user = "admin"
-    foreman_pass = "changeme"
-
-    resource     = RestClient::Resource.new(foreman_url,{ :user => foreman_user, :password =>  foreman_pass,
-                                            :headers => { :accept => :json, :content_type => :json }})
-
     # extend this as required
     searchable_items = %w{ hosts puppetclasses fact_values environments hostgroups }
     item, search = args
     raise Puppet::ParseError, "Foreman: Invalid item to search on: #{item}, must be one of #{searchable_items.join(", ")}." unless searchable_items.include?(item)
 
+    # URL to query
+    foreman_url  = "https://foreman"
+    foreman_user = "admin"
+    foreman_pass = "changeme"
+
+    uri = URI.parse(foreman_url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == 'https'
+
+    path = URI.escape("/#{item}?search=#{search}")
+    req = Net::HTTP::Get.new(path)
+    req.basic_auth(foreman_user, foreman_pass)
+    req['Content-Type'] = 'application/json'
+    req['Accept'] = 'application/json'
+
     begin
-      Timeout::timeout(5) { PSON.parse resource[URI.escape("#{item}?search=#{search}")].get.body }
+      Timeout::timeout(5) { PSON.parse http.request(req).body }
     rescue Exception => e
       raise Puppet::ParseError, "Failed to contact Foreman #{e}"
     end
