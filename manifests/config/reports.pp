1246d0fdd60d458380e6372d995329d5acd6c614
Erik Dalén <dalen@spotify.com>
2012-04-16 15:03:34 +0200

    Separate scripts to be included on puppetmaster
    We shouldn't make the assumption that foreman runs on the same hosts as the
    puppetmaster.
    So this patch separates the scripts that are to be installed on the
    puppetmaster (enc & report script) into the class foreman::puppetmaster
    that can be added to the puppetmaster.
    
    Change-Id: I89b0439d18c1e4032b64c12c337de51958e7f1ab

--- manifests/config/reports.pp
+++ manifests/config/reports.pp
@@ -1,14 +1,5 @@
 class foreman::config::reports {
 
-  # foreman reporter
-  file {"${foreman::params::puppet_basedir}/reports/foreman.rb":
-    mode     => '0444',
-    owner    => 'puppet',
-    group    => 'puppet',
-    content  => template('foreman/foreman-report.rb.erb'),
-    # notify => Service["puppetmaster"],
-  }
-
   cron { 'expire_old_reports':
     command => "(cd ${foreman::params::app_root} && rake reports:expire)",
     minute  => '30',
