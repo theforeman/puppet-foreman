9940d5c0d7af31001d5399fc62ebfb90f84aa754
sam Kottler <sam@kottlerdevelopment.com>
2012-08-03 10:57:06 -0400

    Removed the rackup file symlinking now that it's managed via the packages

--- manifests/config/passenger.pp
+++ manifests/config/passenger.pp
@@ -14,25 +14,10 @@ class foreman::config::passenger {
     refreshonly => true,
     cwd         => $foreman::app_root,
     path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    require     => Class['foreman::install']
   }
 
-  # passenger ~2.10 will not load the app if a config.ru doesn't exist in the
-  # app root. Also, passenger will run as suid to the owner of the config.ru
-  # file.
-
-  if $foreman::package_source != 'stable' {
-    # Config.ru is in the package now
-  } else {
-    file { "$foreman::app_root/config.ru":
-      ensure => link,
-      owner  => $foreman::user,
-      target => "${foreman::app_root}/vendor/rails/railties/dispatches/config.ru",
-    }
-    file { "$foreman::app_root/config/environment.rb":
-      owner   => $foreman::user,
-      require => Class['foreman::install'],
-    }
+  file { ["$foreman::app_root/config.ru", "$foreman::app_root/config/environment.rb"]:
+    owner   => $foreman::user,
+    require => Class['foreman::install'],
   }
-
 }
