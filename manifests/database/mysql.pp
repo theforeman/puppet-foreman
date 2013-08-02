ec6c4a7d70926518f7aa34d52d94605076517e21
Axel Bock <axel.bock@dvag.com>
2013-08-02 14:58:45 +0200

    added raw_db_parameters parameter to foreman

--- manifests/database/mysql.pp
+++ manifests/database/mysql.pp
@@ -5,7 +5,23 @@ class foreman::database::mysql {
     default => $foreman::db_database,
   }
 
-  include mysql, mysql::server, mysql::server::account_security
+  # create database resources, but leave room for user-infused parameters.
+  $dbp = $::foreman::raw_db_parameters
+  $hash = {}
+  $std_params = has_key($dbp, "mysql::server") ? {
+    true    => $dbp['mysql::server'], 
+    default => $hash
+  }
+  $srv_params = has_key($dbp, "mysql") ? {
+    true    => $dbp['mysql'], 
+    default => $hash
+  }
+  create_resources('class', { "mysql::server" => $std_params })
+  create_resources('class', { "mysql"         => $srv_params })
+
+  # has no parameters. simply include.
+  include mysql::server::account_security
+
   mysql::db { $dbname:
     user     => $foreman::db_username,
     password => $foreman::db_password,
--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -15,7 +15,38 @@ class foreman::database::postgresql {
     cwd => '/',
   }
 
-  include postgresql::client, postgresql::server
+  # Postgresql::Params<| |> -> Postgresql::Server<| |>
+  # Postgresql::Params<| |> -> Postgresql::Client<| |>
+
+  # create database resources, but leave room for user-infused parameters.
+  $dbp  = $::foreman::raw_db_parameters
+  if has_key($dbp, "postgresql::params") {
+    validate_hash($dbp['postgresql::params'])
+    $par_params = $dbp['postgresql::params']
+  } else {
+    $par_params = {}
+  }
+  create_resources('class', { 'postgresql::params' => $par_params } )
+
+  if has_key($dbp, "postgresql::server") {
+    validate_hash($dbp['postgresql::server'])
+    $srv_params = $dbp['postgresql::server']
+  } else {
+    $srv_params = {}
+  }
+  create_resources('class', { 'postgresql::server' => $srv_params } )
+
+  if has_key($dbp, "postgresql::client") {
+    validate_hash($dbp['postgresql::client'])
+    $cli_params = $dbp['postgresql::client']
+  } else {
+    $cli_params = {}
+  }
+  create_resources('class', { 'postgresql::client' => $cli_params } )
+
+  Class['postgresql::params'] -> Class['postgresql::server']
+  Class['postgresql::params'] -> Class['postgresql::client']
+
   postgresql::db { $dbname:
     user     => $foreman::db_username,
     password => $password,
