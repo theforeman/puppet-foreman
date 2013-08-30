4e6dd6818ddda2ab146f86b9f04b8c03e5789ddb
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2013-08-30 19:01:20 +0200

    WIP

--- manifests/config/passenger.pp
+++ manifests/config/passenger.pp
@@ -19,27 +19,30 @@ class foreman::config::passenger (
 
   # Set up passenger
   include ::passenger
-  if $scl_prefix {
-    $fragment = "PassengerRuby /usr/bin/${scl_prefix}-ruby
-    PassengerAppRoot ${foreman::app_root}
-    AddDefaultCharset UTF-8"
 
+  if $scl_prefix {
     class {'::passenger::scl':
       prefix => $scl_prefix,
     }
-  } else {
-    $fragment = "PassengerAppRoot ${foreman::app_root}
-    AddDefaultCharset UTF-8"
+  }
+
+  # Workaround so apache::vhost doesn't attempt to create a directory
+  file {"${foreman::app_root}/public": }
+
+  Apache::Vhost {
+    ip            => $listen_interface,
+    servername    => $::fqdn,
+    serveraliases => ['foreman'],
+    docroot       => "${foreman::app_root}/public",
+    priority      => '5',
+    options       => ['none'],
+    require       => Class['foreman::install'],
   }
 
   if $foreman::use_vhost {
     apache::vhost { 'foreman':
-      #ip             => $listen_interface, # FIXME
       port            => 80,
-      docroot         => "${foreman::app_root}/public",
-      priority        => '5',
-      custom_fragment => $fragment,
-      require         => Class['foreman::install'],
+      custom_fragment => template('foreman/apache-fragment.conf.erb'),
     }
   } else {
     file { 'foreman_vhost':
@@ -52,30 +55,14 @@ class foreman::config::passenger (
   }
 
   if $foreman::ssl {
-    if $foreman::use_vhost {
-      apache::vhost { 'foreman-ssl':
-        #ip              => $listen_interface, # FIXME
-        port            => 443,
-        docroot         => "${foreman::app_root}/public",
-        priority        => '5',
-        ssl             => true,
-        ssl_cert        => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
-        ssl_key         => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
-        ssl_chain       => '/var/lib/puppet/ssl/certs/ca.pem',
-        ssl_ca          => '/var/lib/puppet/ssl/certs/ca.pem',
-        custom_fragment => $fragment,
-        require         => Class['foreman::install'],
-      }
-    } else {
-      include apache::mod::ssl
-
-      file { 'foreman_vhost':
-        path    => "${foreman::apache_conf_dir}/foreman-ssl.conf",
-        content => template('foreman/foreman-apache.conf.erb'),
-        mode    => '0644',
-        notify  => Service['httpd'],
-        require => Class['foreman::install'],
-      }
+    apache::vhost { 'foreman-ssl':
+      port            => 443,
+      ssl             => true,
+      ssl_cert        => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+      ssl_key         => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+      ssl_chain       => '/var/lib/puppet/ssl/certs/ca.pem',
+      ssl_ca          => '/var/lib/puppet/ssl/certs/ca.pem',
+      custom_fragment => template('foreman/apache-fragment.conf.erb', 'foreman/apache-fragment-ssl.conf.erb'),
     }
   }
 
