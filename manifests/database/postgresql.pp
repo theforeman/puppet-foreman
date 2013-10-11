04fed3cd83a6e9a9cbad212189d83a28ad0349af
Marek Hulan <mhulan@redhat.com>
2013-10-11 12:04:14 +0200

    Set correct owner of PostgreSQL DB
    With this change we can use foreman-rake db:drop to drop the whole DB.
    It's needed for installer which can drop db and let it recreate using
    existing tools.

--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -16,8 +16,28 @@ class foreman::database::postgresql {
   }
 
   include postgresql::client, postgresql::server
-  postgresql::db { $dbname:
-    user     => $foreman::db_username,
-    password => $password,
+  $grant = 'ALL'
+  # TODO copied from puppetlabs-postgresql 2.3.0 manifests/db.pp
+  # should be removed by db once they expose owner parameter
+  postgresql::database { $dbname:
+    charset     => $postgresql::params::charset,
+    tablespace  => undef,
+    require     => Class['postgresql::server'],
+    locale      => $postgresql::params::locale,
+    owner       => $dbname,
+  }
+
+  if ! defined(Postgresql::Database_user[$user]) {
+    postgresql::database_user { $dbname:
+      password_hash   => $password,
+      require         => Postgresql::Database[$dbname],
+    }
+  }
+
+  postgresql::database_grant { "GRANT ${$foreman::db_username} - ${grant} - ${dbname}":
+    privilege       => $grant,
+    db              => $dbname,
+    role            => $foreman::db_username,
+    require         => [Postgresql::Database[$dbname], Postgresql::Database_user[$foreman::db_username]],
   }
 }
