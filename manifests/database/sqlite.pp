be17ceff02c15632ef224deb9b0d5d3c4c0d79d7
Marek Hulan <mhulan@redhat.com>
2013-10-11 12:05:07 +0200

    Run migrations when using sqlite
    Fixes the issue when migrations are not run on sqlite
    since it did not emit any refresh notification.

--- manifests/database/sqlite.pp
+++ manifests/database/sqlite.pp
@@ -1,4 +1,7 @@
 # Set up the foreman database using sqlite
 class foreman::database::sqlite {
-  # no-op
+  exec { 'create':
+    command => '/usr/sbin/foreman-rake db:create',
+    creates => "${foreman::app_root}/db/production.sqlite3",
+  }
 }
