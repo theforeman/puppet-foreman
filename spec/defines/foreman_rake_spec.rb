9344250e7fc70818f1e646d2234556a8d4ffeb60
Dominic Cleal <dcleal@redhat.com>
2013-11-27 10:46:19 +0000

    refs #3752 - run db:seed after DB migration

---
+++ spec/defines/foreman_rake_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'foreman::rake' do
+  let :default_facts do
+    {
+      :concat_basedir           => '/tmp',
+      :interfaces               => '',
+      :postgres_default_version => '8.4',
+    }
+  end
+
+  let :pre_condition do
+    "class { 'foreman':
+      db_manage => false,
+     }"
+  end
+
+  let(:title) { 'db:migrate' }
+
+  context 'on RedHat' do
+    let :facts do
+      default_facts.merge({
+        :operatingsystem        => 'RedHat',
+        :operatingsystemrelease => '6.4',
+        :osfamily               => 'RedHat',
+      })
+    end
+
+    it { should contain_exec('foreman-rake-db:migrate').with({
+      'command'     => '/usr/sbin/foreman-rake db:migrate',
+      'user'        => 'foreman',
+      'environment' => 'HOME=/usr/share/foreman',
+      'logoutput'   => 'on_failure',
+      'refreshonly' => true,
+    })}
+  end
+end
