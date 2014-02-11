6a63be481d1026abcce9cef1d84797c20ccb1275
Greg Sutcliffe <gsutclif@redhat.com>
2014-02-11 18:31:41 +0000

    Refs #3308 - Ensure plugins are installed after core

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -2,7 +2,8 @@ define foreman::plugin(
   $package = "${foreman::plugin_prefix}${title}"
 ) {
   package { $package:
-    ensure => installed,
-    notify => Class['foreman::service'],
+    ensure  => installed,
+    require => Class['foreman::config'],
+    notify  => Class['foreman::service'],
   }
 }
