a72a1bb4cfaa5dd6cbee0ca23eaa674e70b6dc2d
Greg Sutcliffe <gsutclif@redhat.com>
2014-02-26 12:09:23 +0000

    Refs #3308 Handle underscores vs hyphens with regsubst

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,7 +1,16 @@
 define foreman::plugin(
   $package = "${foreman::plugin_prefix}${title}"
 ) {
-  package { $package:
+  # Debian gem2deb converts underscores to hyphens
+  case $::osfamily {
+    Debian: {
+      $real_package = regsubst($package,'_','-','G')
+    }
+    default: {
+      $real_package = $package
+    }
+  }
+  package { $real_package:
     ensure => installed,
     notify => Class['foreman::service'],
   }
