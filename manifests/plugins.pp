12e5aedcc40a577afe81958c94e9d349af9bc52d
Simon Murray <simon.murray@datacentred.co.uk>
2014-03-31 15:47:36 +0100

    foreman hipchat notifier

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,4 +1,7 @@
-class foreman::plugins {
+class foreman::plugins (
+  $foreman_hipchat_token = undef,
+  $foreman_hipchat_room  = undef,
+) {
 
   package { 'git':
     ensure => present,
@@ -12,6 +15,34 @@ class foreman::plugins {
     cwd         => $foreman::app_root,
     command     => '/usr/bin/bundle update',
     refreshonly => true,
+  }
+
+  $home = '/usr/share/foreman'
+
+  package { 'python-pip':
+    ensure => present,
+  } ->
+  package { 'python-simple-hipchat':
+    ensure  => present,
+    provider => 'pip',
+  }
+
+  file { [ "${home}/config/hooks",
+           "${home}/config/hooks/host",
+           "${home}/config/hooks/host/managed",
+           "${home}/config/hooks/host/managed/before_provision"
+  ]:
+    ensure => directory,
+    owner  => 'foreman',
+    group  => 'foreman',
+    mode   => '0770',
+  } ->
+  file { "${home}/config/hooks/host/managed/before_provision/10_hipchat.py":
+    ensure  => file,
+    owner   => 'foreman',
+    group   => 'foreman',
+    mode    => '0770',
+    content => template('foreman/10_hipchat.py.erb'),
   }
 
 }
