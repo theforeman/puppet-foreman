0beadb4a79f01e0536dbd9d524ef83073ead747e
Sean Handley <sean.handley@gmail.com>
2014-07-14 17:17:02 +0100

    Place the scripts into foreman hooks.

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,6 +1,7 @@
 class foreman::plugins (
   $foreman_hipchat_token = undef,
   $foreman_hipchat_room  = undef,
+  $foreman_icinga_token = undef,
 ) {
 
   package { 'git':
@@ -30,7 +31,8 @@ class foreman::plugins (
   file { [ "${home}/config/hooks",
            "${home}/config/hooks/host",
            "${home}/config/hooks/host/managed",
-           "${home}/config/hooks/host/managed/before_provision"
+           "${home}/config/hooks/host/managed/before_provision",
+           "${home}/config/hooks/host/managed/after_build"
   ]:
     ensure => directory,
     owner  => 'foreman',
@@ -43,6 +45,20 @@ class foreman::plugins (
     group   => 'foreman',
     mode    => '0770',
     content => template('foreman/10_hipchat.py.erb'),
+  } ->
+  file { "${home}/config/hooks/host/managed/before_provision/20_enable_icinga.rb":
+    ensure  => file,
+    owner   => 'foreman',
+    group   => 'foreman',
+    mode    => '0770',
+    content => template('foreman/20_enable_icinga.rb.erb'),
+  } ->
+  file { "${home}/config/hooks/host/managed/after_build/10_disable_icinga.rb":
+    ensure  => file,
+    owner   => 'foreman',
+    group   => 'foreman',
+    mode    => '0770',
+    content => template('foreman/10_disable_icinga.rb.erb'),
   }
 
 }
