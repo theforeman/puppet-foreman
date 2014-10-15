1b656a5335252275a29e1e2b3a72bd17754d6cc4
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-15 16:22:32 +0200

    Refactor Sssd facts to gain performance and use a util library

--- lib/facter/facts.rb
+++ lib/facter/facts.rb
@@ -1,3 +1,5 @@
+require 'facter/util/sssd'
+
 # == Fact: default_ipa_realm
 # == Fact: default_ipa_server
 # == Fact: sssd_services
@@ -5,50 +7,33 @@
 # == Fact: sssd_allowed_uids
 # == Fact: sssd_user_attributes
 #
-require 'augeas'
 Facter.add(:default_ipa_realm) do
   setcode do
-    Augeas::open('/', nil, Augeas::NO_MODL_AUTOLOAD) do |aug|
-      aug.transform(:lens => "Puppet.lns", :incl => "/etc/ipa/default.conf")
-      aug.load
-      aug.get("/files/etc/ipa/default.conf/global/realm")
-    end
+    Facter::Util::Sssd.ipa_value('global/realm')
   end
 end
 Facter.add(:default_ipa_server) do
   setcode do
-    Augeas::open('/', nil, Augeas::NO_MODL_AUTOLOAD) do |aug|
-      aug.transform(:lens => "Puppet.lns", :incl => "/etc/ipa/default.conf")
-      aug.load
-      aug.get("/files/etc/ipa/default.conf/global/server")
-    end
+    Facter::Util::Sssd.ipa_value('global/server')
   end
 end
 Facter.add(:sssd_services) do
   setcode do
-    Augeas::open do |aug|
-      aug.get("/files/etc/sssd/sssd.conf/target[.='sssd']/services")
-    end
+    Facter::Util::Sssd.sssd_value('target[.="sssd"]/services')
   end
 end
 Facter.add(:sssd_ldap_user_extra_attrs) do
   setcode do
-    Augeas::open do |aug|
-      aug.get("/files/etc/sssd/sssd.conf/target[.=~regexp('domain/.*')][1]/ldap_user_extra_attrs")
-    end
+    Facter::Util::Sssd.sssd_value('target[.=~regexp("domain/.*")][1]/ldap_user_extra_attrs')
   end
 end
 Facter.add(:sssd_allowed_uids) do
   setcode do
-    Augeas::open do |aug|
-      aug.get("/files/etc/sssd/sssd.conf/target[.='ifp']/allowed_uids")
-    end
+    Facter::Util::Sssd.sssd_value('target[.="ifp"]/allowed_uids')
   end
 end
 Facter.add(:sssd_user_attributes) do
   setcode do
-    Augeas::open do |aug|
-      aug.get("/files/etc/sssd/sssd.conf/target[.='ifp']/user_attributes")
-    end
+    Facter::Util::Sssd.sssd_value('target[.="ifp"]/user_attributes')
   end
 end
