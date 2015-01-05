b969c150ba51bf4978e0bd197b11b97bb5966ca7
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-01-05 10:17:45 +0100

    Use rspec-puppet-facts

--- spec/classes/foreman_database_spec.rb
+++ spec/classes/foreman_database_spec.rb
@@ -1,75 +1,46 @@
 require 'spec_helper'
 
 describe 'foreman::install' do
-  let :default_facts do
-    {
-      :concat_basedir => '/tmp',
-      :interfaces     => '',
-    }
-  end
 
-  context 'RedHat' do
-    let :facts do
-      default_facts.merge({
-        :operatingsystem        => 'RedHat',
-        :operatingsystemrelease => '6.4',
-        :osfamily               => 'RedHat',
-      })
-    end
-
-    describe 'without parameters' do
-      let :pre_condition do
-        "class {'foreman':}"
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :concat_basedir => '/tmp',
+          :interfaces     => '',
+        })
       end
 
-      it { should contain_class('foreman::database::postgresql') }
+      describe 'without parameters' do
+        let :pre_condition do
+          "class {'foreman':}"
+        end
 
-      it { should contain_foreman_config_entry('db_pending_migration') }
-      it { should contain_foreman__rake('db:migrate') }
-      it { should contain_foreman_config_entry('db_pending_seed') }
-      it { should contain_foreman__rake('db:seed') }
-      it { should contain_foreman__rake('apipie:cache') }
-    end
+        it { should contain_class('foreman::database::postgresql') }
 
-    describe 'with seed parameters' do
-      let :pre_condition do
-        "class {'foreman':
+        it { should contain_foreman_config_entry('db_pending_migration') }
+        it { should contain_foreman__rake('db:migrate') }
+        it { should contain_foreman_config_entry('db_pending_seed') }
+        it { should contain_foreman__rake('db:seed') }
+        it { should contain_foreman__rake('apipie:cache') }
+      end
+
+      describe 'with seed parameters' do
+        let :pre_condition do
+          "class {'foreman':
            admin_username => 'joe',
            admin_password => 'secret',
          }"
-      end
+        end
 
-      it {
-        should contain_foreman__rake('db:seed').
+        it {
+          should contain_foreman__rake('db:seed').
           with_environment({
             'SEED_ADMIN_USER'     => 'joe',
             'SEED_ADMIN_PASSWORD' => 'secret',
           })
-      }
-    end
-  end
-
-  context 'on debian' do
-    let :facts do
-      default_facts.merge({
-        :operatingsystem        => 'Debian',
-        :operatingsystemrelease => 'wheezy',
-        :osfamily               => 'Debian',
-      })
-    end
-
-    describe 'without parameters' do
-      let :pre_condition do
-        "class {'foreman':}"
+        }
       end
-
-      it { should contain_class('foreman::database::postgresql') }
-
-      it { should contain_foreman_config_entry('db_pending_migration') }
-      it { should contain_foreman__rake('db:migrate') }
-      it { should contain_foreman_config_entry('db_pending_seed') }
-      it { should contain_foreman__rake('db:seed') }
-      it { should contain_foreman__rake('apipie:cache') }
     end
   end
 end
--- spec/classes/foreman_install_repos_extra_spec.rb
+++ spec/classes/foreman_install_repos_extra_spec.rb
@@ -1,96 +1,72 @@
 require 'spec_helper'
 
 describe 'foreman::install::repos::extra' do
-  describe 'when EL repos are fully enabled' do
-    let(:params) do
-      {
-        :configure_scl_repo  => true,
-        :configure_epel_repo => true,
-      }
-    end
 
-    context 'RHEL 6' do
-      let :facts do
-        {
-          :operatingsystem        => 'RedHat',
-          :operatingsystemrelease => '6.4',
-          :osfamily               => 'RedHat',
-        }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :concat_basedir => '/tmp',
+        })
       end
 
-      it { should contain_yumrepo('epel').with({
-        :mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
-        :gpgcheck   => 1,
-        :gpgkey     => 'https://fedoraproject.org/static/0608B895.txt',
-      }) }
-      it { should contain_package('foreman-release-scl') }
-    end
+      describe 'when repos are fully enabled' do
+        case facts[:osfamily]
+        when 'Debian'
+          if facts[:operatingsystem] == 'Ubuntu'
+            let(:params) do
+              {
+                :configure_brightbox_repo => true,
+              }
+            end
 
-    context 'RHEL 7' do
-      let :facts do
-        {
-          :operatingsystem        => 'RedHat',
-          :operatingsystemrelease => '7.0',
-          :osfamily               => 'RedHat',
-        }
+            it { should contain_class('apt') }
+            it { should contain_apt__ppa('ppa:brightbox/ruby-ng') }
+            it { should contain_alternatives('ruby') }
+            it { should contain_alternatives('gem') }
+          end
+        when 'RedHat'
+          if facts[:operatingsystem] != 'Fedora'
+            let(:params) do
+              {
+                :configure_scl_repo  => true,
+                :configure_epel_repo => true,
+              }
+            end
+
+            let(:gpgkey) do
+              case facts[:operatingsystemmajrelease]
+              when '6'
+                '0608B895'
+              when '7'
+                '352C64E5'
+              end
+            end
+
+            it { should contain_yumrepo('epel').with({
+              :mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-#{facts[:operatingsystemmajrelease]}&arch=$basearch",
+              :gpgcheck   => 1,
+              :gpgkey     => "https://fedoraproject.org/static/#{gpgkey}.txt",
+            }) }
+            it { should contain_package('foreman-release-scl') }
+          end
+        end
       end
 
-      it { should contain_yumrepo('epel').with({
-        :mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=$basearch',
-        :gpgcheck   => 1,
-        :gpgkey     => 'https://fedoraproject.org/static/352C64E5.txt',
-      }) }
-      it { should contain_package('foreman-release-scl') }
-    end
-  end
+      describe 'when fully disabled' do
+        let(:params) do
+          {
+            :configure_scl_repo       => false,
+            :configure_epel_repo      => false,
+            :configure_brightbox_repo => false,
+          }
+        end
 
-  describe 'when deb repos are fully enabled' do
-    let(:params) do
-      {
-        :configure_brightbox_repo  => true,
-      }
-    end
-
-    context 'Ubuntu' do
-      let :facts do
-        {
-          :lsbdistid              => 'ubuntu',
-          :lsbdistcodename        => 'precise',
-          :operatingsystem        => 'Ubuntu',
-          :operatingsystemrelease => '12.04',
-          :osfamily               => 'Debian',
-        }
+        it { should_not contain_yumrepo('epel') }
+        it { should_not contain_package('foreman-release-scl') }
+        it { should_not contain_class('apt') }
+        it { should have_apt__ppa_resource_count(0) }
       end
-
-      it { should contain_class('apt') }
-      it { should contain_apt__ppa('ppa:brightbox/ruby-ng') }
-      it { should contain_alternatives('ruby') }
-      it { should contain_alternatives('gem') }
-    end
-  end
-
-  describe 'when fully disabled' do
-    let(:params) do
-      {
-        :configure_scl_repo       => false,
-        :configure_epel_repo      => false,
-        :configure_brightbox_repo => false,
-      }
-    end
-
-    context 'RHEL 6' do
-      let :facts do
-        {
-          :operatingsystem        => 'RedHat',
-          :operatingsystemrelease => '6.4',
-          :osfamily               => 'RedHat',
-        }
-      end
-
-      it { should_not contain_yumrepo('epel') }
-      it { should_not contain_package('foreman-release-scl') }
-      it { should_not contain_class('apt') }
-      it { should have_apt__ppa_resource_count(0) }
     end
   end
 end
