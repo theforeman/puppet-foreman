a34f4a4309d91eb6b68d0f6766b8b025da16d485
Stephen Benjamin <stbenjam@redhat.com>
2015-11-13 07:31:50 -0500

    Install tasks with remote_execution, chef and salt
    (cherry picked from commit d3b81f7f78e5f6299d366d26af27293e2007d30e)

--- spec/classes/foreman_plugin_remote_execution_spec.rb
+++ spec/classes/foreman_plugin_remote_execution_spec.rb
@@ -4,5 +4,6 @@ describe 'foreman::plugin::remote_execution' do
   on_supported_os.each do |_os, facts|
     let(:facts) { facts }
     it { should contain_foreman__plugin('remote_execution') }
+    it { should contain_foreman__plugin('tasks') }
   end
 end
