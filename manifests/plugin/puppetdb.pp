f85c7e4481803cb54ce09368c08703da7b8b159e
Joseph Yaworski <joseph.yaworski@fireeye.com>
2015-12-22 08:29:31 -0500

    Allow puppetdb plugin config settings to be managed.
    closes GH-383

--- manifests/plugin/puppetdb.pp
+++ manifests/plugin/puppetdb.pp
@@ -6,10 +6,31 @@
 #
 # $package:: Package name to install, use ruby193-rubygem-puppetdb_foreman on Foreman 1.8/1.9 on EL
 #
-class foreman::plugin::puppetdb(
-  $package = $foreman::plugin::puppetdb::params::package,
+# $address:: Address of puppetdb API. Defaults to 'https://localhost:8081/v2/commands'
+#
+# $dashboard_address:: Address of puppetdb dashboard. Defaults to 'http://localhost:8080/dashboard'
+#
+class foreman::plugin::puppetdb (
+  $package           = $foreman::plugin::puppetdb::params::package,
+  $address           = $foreman::plugin::puppetdb::params::address,
+  $dashboard_address = $foreman::plugin::puppetdb::params::dashboard_address,
 ) inherits foreman::plugin::puppetdb::params {
-  foreman::plugin {'puppetdb':
+
+  validate_string($package, $address, $dashboard_address)
+
+  foreman::plugin { 'puppetdb':
     package => $package,
+  }
+  ->
+  foreman_config_entry { 'puppetdb_enabled':
+    value => true,
+  }
+  ->
+  foreman_config_entry { 'puppetdb_address':
+    value => $address,
+  }
+  ->
+  foreman_config_entry { 'puppetdb_dashboard_address':
+    value => $dashboard_address,
   }
 }
