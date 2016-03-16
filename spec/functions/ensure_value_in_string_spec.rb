85b0dee0f94f70b50e8d1a785cb28a644c876104
Michael Moll <kvedulv@kvedulv.de>
2016-03-16 17:52:03 +0100

    modernize deprecated rspec syntax
    Closes GH-415

--- spec/functions/ensure_value_in_string_spec.rb
+++ spec/functions/ensure_value_in_string_spec.rb
@@ -4,7 +4,7 @@ describe 'ensure_value_in_string' do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
   it 'should exist' do
-    Puppet::Parser::Functions.function('ensure_value_in_string').should == 'function_ensure_value_in_string'
+    expect(Puppet::Parser::Functions.function('ensure_value_in_string')).to eq 'function_ensure_value_in_string'
   end
 
   it 'should throw an error with bad number of arguments' do
@@ -22,25 +22,25 @@ describe 'ensure_value_in_string' do
   end
 
   it 'should append values to the end' do
-    scope.function_ensure_value_in_string(['one,two', ['three']]).should eq('one,two,three')
+    expect(scope.function_ensure_value_in_string(['one,two', ['three']])).to eq 'one,two,three'
   end
 
   it 'should append values to the end in same order' do
-    scope.function_ensure_value_in_string(['one,two', ['three', 'four']]).should eq('one,two,three,four')
+    expect(scope.function_ensure_value_in_string(['one,two', ['three', 'four']])).to eq 'one,two,three,four'
   end
 
   it 'should not append values existing in original string' do
-    scope.function_ensure_value_in_string(['one,two', ['two', 'three']]).should eq('one,two,three')
-    scope.function_ensure_value_in_string(['one,two', ['one', 'three']]).should eq('one,two,three')
-    scope.function_ensure_value_in_string(['one,two', ['one', 'two']]).should eq('one,two')
+    expect(scope.function_ensure_value_in_string(['one,two', ['two', 'three']])).to eq 'one,two,three'
+    expect(scope.function_ensure_value_in_string(['one,two', ['one', 'three']])).to eq 'one,two,three'
+    expect(scope.function_ensure_value_in_string(['one,two', ['one', 'two']])).to eq 'one,two'
   end
 
   it 'should append even to empty strings' do
-    scope.function_ensure_value_in_string(['', ['two', 'three']]).should eq('two,three')
+    expect(scope.function_ensure_value_in_string(['', ['two', 'three']])).to eq 'two,three'
   end
 
   it 'should append even empty array' do
-    scope.function_ensure_value_in_string(['one,two', []]).should eq('one,two')
+    expect(scope.function_ensure_value_in_string(['one,two', []])).to eq 'one,two'
   end
 
   it 'should not allow using wrong types and undefined values' do
@@ -58,10 +58,10 @@ describe 'ensure_value_in_string' do
   end
 
   it 'should ignore whitespaces but preserve it' do
-    scope.function_ensure_value_in_string(['one, two,   three    ,    four', ['five']]).should eq('one, two,   three    ,    four,five')
+    expect(scope.function_ensure_value_in_string(['one, two,   three    ,    four', ['five']])).to eq 'one, two,   three    ,    four,five'
   end
 
   it 'should accept third argument as a custom separator' do
-    scope.function_ensure_value_in_string(['one,two', ['two', 'three'], ', ']).should eq('one,two, three')
+    expect(scope.function_ensure_value_in_string(['one,two', ['two', 'three'], ', '])).to eq 'one,two, three'
   end
 end
--- spec/unit/foreman_external_node_spec.rb
+++ spec/unit/foreman_external_node_spec.rb
@@ -35,29 +35,29 @@ describe 'foreman_external_node' do
 
     req = enc.generate_fact_request('fake.host.fqdn.com',"#{static_fixture_path}/fake.host.fqdn.com.yaml")
     enc.upload_facts('fake.host.fqdn.com',req)
-    webstub.should have_been_requested
+    expect(webstub).to have_been_requested
 
     # test pushing facts async
     http_fact_requests = []
     http_fact_requests << ['fake.host.fqdn.com', req]
     enc.upload_facts_parallel(http_fact_requests)
 
-    webstub.should have_been_requested.times(2)
+    expect(webstub).to have_been_requested.times(2)
 
     http_fact_requests << ['fake.host.fqdn.com', req]
     http_fact_requests << ['fake.host.fqdn.com', req]
     enc.upload_facts_parallel(http_fact_requests)
 
-    webstub.should have_been_requested.times(4)
+    expect(webstub).to have_been_requested.times(4)
   end
 
   it "should have the correct certname and hostname" do
     # fake2 does not appear in the fixture, so we know it
     # must be preferring the passed-in certname from ARGV
     hash = enc.build_body('fake2.host.fqdn.com',"#{static_fixture_path}/fake.host.fqdn.com.yaml")
-    hash['certname'].should eql('fake2.host.fqdn.com')
-    hash['name'].should eql('fake.host.fqdn.com')
-    hash['facts'].should be_a(Hash)
+    expect(hash['certname']).to eql 'fake2.host.fqdn.com'
+    expect(hash['name']).to eql 'fake.host.fqdn.com'
+    expect(hash['facts']).to be_a(Hash)
   end
 
 end
