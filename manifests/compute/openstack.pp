e8917aa39843b87f35455e645529b506dd9ada9f
Dominic Cleal <dominic@cleal.org>
2016-03-31 15:43:56 +0100

    Change openstack/rackspace compute resource packages
    foreman-openstack/rackspace subpackages are provided on Foreman 1.12,
    per ticket #14368. Compatibility with 1.11 and older is provided through
    the $package parameter.
    
    closes GH-422

--- manifests/compute/openstack.pp
+++ manifests/compute/openstack.pp
@@ -1,4 +1,20 @@
+# = Foreman OpenStack compute resource support
+#
 # Provides support for OpenStack compute resources
-class foreman::compute::openstack {
-  include ::foreman::compute::foreman_compute
+#
+# === Parameters:
+#
+# $version::  Package version to install, defaults to installed
+#
+# $package::  Package name to install, use foreman-compute on Foreman 1.11 or older
+#
+class foreman::compute::openstack($package = 'foreman-openstack', $version = 'installed') {
+  if $package == 'foreman-compute' {
+    include ::foreman::compute::foreman_compute
+  } else {
+    package { $package:
+      ensure => $version,
+      tag    => [ 'foreman-compute', ],
+    }
+  }
 }
