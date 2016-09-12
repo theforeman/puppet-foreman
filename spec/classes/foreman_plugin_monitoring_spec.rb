910f59ca9b15062fde25d13bd67e6736b8d6f5e7
Timo Goebel <timo.goebel@dm.de>
2016-09-12 15:01:39 +0200

    add plugin foreman_monitoring

---
+++ spec/classes/foreman_plugin_monitoring_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'foreman::plugin::monitoring' do
+  let(:facts) do
+    on_supported_os['redhat-7-x86_64']
+  end
+
+  let(:pre_condition) { 'include foreman' }
+
+  it { should contain_foreman__plugin('monitoring') }
+end
