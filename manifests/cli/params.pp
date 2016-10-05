ef7e98d28cfca484d990e40118ae63907a9119fa
Marek Hulan <mhulan@redhat.com>
2016-10-05 15:47:27 +0200

    Fixes #16802 - enable hammer openscap plugin installation

--- manifests/cli/params.pp
+++ manifests/cli/params.pp
@@ -7,4 +7,43 @@ class foreman::cli::params {
   $password = undef
   $refresh_cache = false
   $request_timeout = 120
+
+  # OS specific paths
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystem {
+        'fedora': {
+          $hammer_plugin_prefix = 'rubygem-hammer_cli_'
+        }
+        default: {
+          $hammer_plugin_prefix = 'tfm-rubygem-hammer_cli_'
+        }
+      }
+    }
+    'Debian': {
+      $hammer_plugin_prefix = 'ruby-hammer-cli-'
+    }
+    'Linux': {
+      case $::operatingsystem {
+        'Amazon': {
+          $hammer_plugin_prefix = 'tfm-rubygem-hammer_cli_'
+        }
+        default: {
+          fail("${::hostname}: This module does not support operatingsystem ${::operatingsystem}")
+        }
+      }
+    }
+    /(ArchLinux|Suse)/: {
+      $hammer_plugin_prefix = undef
+    }
+    /^(FreeBSD|DragonFly)$/: {
+      $hammer_plugin_prefix = undef
+    }
+    'windows': {
+      $hammer_plugin_prefix = undef
+    }
+    default: {
+      fail("${::hostname}: This module does not support osfamily ${::osfamily}")
+    }
+  }
 }
