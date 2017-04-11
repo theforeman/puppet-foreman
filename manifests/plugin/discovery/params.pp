82810d25fc280d6d7b663931bfaba0cc5d5cfc68
Dominic Cleal <dominic@cleal.org>
2017-04-11 11:56:56 +0100

    Replace tftp::params::root use with Puppet lookup

--- manifests/plugin/discovery/params.pp
+++ manifests/plugin/discovery/params.pp
@@ -1,9 +1,7 @@
 # Default parameters for foreman::plugin::discovery
 class foreman::plugin::discovery::params {
-  include ::tftp::params
-
   $install_images = false
-  $tftp_root      = $::tftp::params::root
+  $tftp_root      = lookup('tftp::root')
   $source_url     = 'http://downloads.theforeman.org/discovery/releases/latest/'
   $image_name     = 'fdi-image-latest.tar'
 }
