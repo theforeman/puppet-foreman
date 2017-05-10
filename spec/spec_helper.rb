e235d49aae8648e8994ebae01a9056e702370e34
Michael Moll <kvedulv@kvedulv.de>
2017-05-10 12:39:42 +0200

    minor modulesync updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,22 +44,25 @@ def on_os_under_test
 end
 
 def get_content(subject, title)
+  is_expected.to contain_file(title)
   content = subject.resource('file', title).send(:parameters)[:content]
   content.split(/\n/).reject { |line| line =~ /(^#|^$|^\s+#)/ }
 end
 
 def verify_exact_contents(subject, title, expected_lines)
-  expect(get_content(subject, title)).to eq(expected_lines)
+  expect(get_content(subject, title)).to match_array(expected_lines)
 end
 
 def verify_concat_fragment_contents(subject, title, expected_lines)
+  is_expected.to contain_concat__fragment(title)
   content = subject.resource('concat::fragment', title).send(:parameters)[:content]
-  expect(content.split("\n") & expected_lines).to eq(expected_lines)
+  expect(content.split("\n") & expected_lines).to match_array(expected_lines)
 end
 
 def verify_concat_fragment_exact_contents(subject, title, expected_lines)
+  is_expected.to contain_concat__fragment(title)
   content = subject.resource('concat::fragment', title).send(:parameters)[:content]
-    expect(content.split(/\n/).reject { |line| line =~ /(^#|^$|^\s+#)/ }).to eq(expected_lines)
+  expect(content.split(/\n/).reject { |line| line =~ /(^#|^$|^\s+#)/ }).to match_array(expected_lines)
 end
 
 def static_fixture_path
