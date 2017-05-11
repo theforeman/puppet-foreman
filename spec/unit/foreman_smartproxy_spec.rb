a2c4df2d21c63e1244eb7c9ec7d20c24e73d39e9
Dominic Cleal <dominic@cleal.org>
2017-05-11 10:55:26 +0100

    Improve formatting of foreman_smartproxy feature lists
    Originally:
      change from ["Logs", "Puppet CA", "TFTP"] to Puppet Puppet CA TFTP Logs failed
    
    Now:
      change from ["Logs", "Puppet CA", "TFTP"] to ["Logs", "Puppet", "Puppet CA", "TFTP"] failed

--- spec/unit/foreman_smartproxy_spec.rb
+++ spec/unit/foreman_smartproxy_spec.rb
@@ -8,5 +8,7 @@ describe Puppet::Type.type(:foreman_smartproxy) do
     it { expect(described_class.new(name: 's', features: ['TFTP', 'Logs']).property(:features).insync?(['Logs', 'TFTP'])).to eq(true) }
     it { expect(described_class.new(name: 's', features: ['TFTP', 'Logs']).property(:features).insync?(['Logs'])).to eq(false) }
     it { expect(described_class.new(name: 's', features: ['TFTP', 'Logs']).property(:features).insync?([])).to eq(false) }
+    it { expect(described_class.new(name: 's').property(:features).is_to_s(['Foo', 'Bar'])).to eq('["Bar", "Foo"]') }
+    it { expect(described_class.new(name: 's').property(:features).should_to_s(['Foo', 'Bar'])).to eq('["Bar", "Foo"]') }
   end
 end
