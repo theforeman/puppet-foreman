2d3e6c528dbda977f4603382c18373bc36a5c84f
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-08-31 17:14:27 +0200

    Remove discovery image downloading
    The image downloading is tied to tftp and handled by the proxy. Since
    puppet-foreman_proxy is already able to download images there's no sense
    in duplicating the code here.
    
    Based on https://github.com/theforeman/puppet-foreman/pull/486
    
    Closes GH-580
    Closes GH-581

--- manifests/plugin/discovery.pp
+++ manifests/plugin/discovery.pp
@@ -2,37 +2,7 @@
 #
 # This class installs discovery plugin and images
 #
-# === Parameters:
-#
-# $install_images::  should the installer download and setup discovery images
-#                    for you? the average size is few hundreds of MB
-#
-# $tftp_root::       TFTP root to install image into
-#
-# $source_url::      source URL to download from
-#
-# $image_name::      tarball with images
-#
-class foreman::plugin::discovery (
-  Boolean $install_images = $::foreman::plugin::discovery::params::install_images,
-  Stdlib::Absolutepath $tftp_root = $::foreman::plugin::discovery::params::tftp_root,
-  Stdlib::HTTPUrl $source_url = $::foreman::plugin::discovery::params::source_url,
-  String $image_name = $::foreman::plugin::discovery::params::image_name,
-) inherits foreman::plugin::discovery::params {
+class foreman::plugin::discovery {
   foreman::plugin {'discovery':
-  }
-
-  if $install_images {
-    $tftp_root_clean = regsubst($tftp_root, '/$', '')
-
-    foreman::remote_file {"${tftp_root_clean}/boot/${image_name}":
-      remote_location => "${source_url}${image_name}",
-      mode            => '0644',
-    } ~> exec { "untar ${image_name}":
-      command => "tar xf ${image_name}",
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      cwd     => "${tftp_root_clean}/boot",
-      creates => "${tftp_root_clean}/boot/fdi-image/initrd0.img",
-    }
   }
 }
--- manifests/plugin/discovery/params.pp
+++
@@ -1,7 +0,0 @@
-# Default parameters for foreman::plugin::discovery
-class foreman::plugin::discovery::params {
-  $install_images = false
-  $tftp_root      = lookup('tftp::root')
-  $source_url     = 'http://downloads.theforeman.org/discovery/releases/latest/'
-  $image_name     = 'fdi-image-latest.tar'
-}
