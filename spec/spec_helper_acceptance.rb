9530c3d1925b37921de6a5d2f772da1f37805e6d
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-09-02 21:35:21 +0200

    msync: beaker install helper, new travis matrix, facter 2.5

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,25 +1,20 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'foreman')
     hosts.each do |host|
-      ["puppet-extlib", "puppetlabs-apt"].each do |mod|
-        on host, puppet('module', 'install', mod), { :acceptable_exit_codes => [0] }
-      end
-
       if fact_on(host, 'osfamily') == 'RedHat'
         # don't delete downloaded rpm for use with BEAKER_provision=no +
         # BEAKER_destroy=no
@@ -39,4 +34,13 @@ shared_examples 'a idempotent resource' do
   it 'applies a second time without changes' do
     apply_manifest(pp, catch_changes: true)
   end
+end
+
+shared_examples 'the example' do |name|
+  let(:pp) do
+    path = File.join(File.dirname(File.dirname(__FILE__)), 'examples', name)
+    File.read(path)
+  end
+
+  include_examples 'a idempotent resource'
 end
