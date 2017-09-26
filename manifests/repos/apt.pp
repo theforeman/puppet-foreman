ba8b0acdfb279c925c2d9de90a11185a754aefea
Patrick Ruckstuhl <patrick@ch.tario.org>
2017-09-26 10:50:39 +0200

    Drop explicit source for pgp key (#594)
    Fixes GH-593

--- manifests/repos/apt.pp
+++ manifests/repos/apt.pp
@@ -5,10 +5,7 @@ define foreman::repos::apt ($repo) {
 
   Apt::Source {
     location => 'http://deb.theforeman.org/',
-    key      => {
-      id     => 'AE0AF310E2EA96B6B6F4BD726F8600B9563278F6',
-      source => 'https://pgp.mit.edu/pks/lookup?op=get&search=0x6F8600B9563278F6',
-    },
+    key      => 'AE0AF310E2EA96B6B6F4BD726F8600B9563278F6',
     include  => {
       src => false,
     },
