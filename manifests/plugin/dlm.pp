b153018739074486f48bedf97ef3ba823b80cd8f
cocker-cc <36535791+cocker-cc@users.noreply.github.com>
2018-06-22 14:18:52 +0200

    Adding dlm Plugin (#646)

---
+++ manifests/plugin/dlm.pp
@@ -0,0 +1,5 @@
+# Installs foreman_dlm plugin
+class foreman::plugin::dlm {
+  foreman::plugin {'dlm':
+  }
+}
---
+++ spec/classes/plugin/dlm_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'foreman::plugin::dlm' do
+  include_examples 'basic foreman plugin tests', 'dlm'
+end
