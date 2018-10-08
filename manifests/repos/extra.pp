23669ae189bf8a7d0f2f1287dbea4b5f65bf9d98
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-10-08 15:39:14 +0200

    Refactor extras repo handling
    This drops support for EL6 by only having the URL for the EL6 GPG key.
    
    It also allows setting the package state of foreman-release-scl.

--- manifests/repos/extra.pp
+++ manifests/repos/extra.pp
@@ -1,28 +1,23 @@
 # Configure thirdparty repos
 class foreman::repos::extra(
-  $configure_epel_repo      = $::foreman::configure_epel_repo,
-  $configure_scl_repo       = $::foreman::configure_scl_repo,
+  Boolean $configure_epel_repo = $::foreman::configure_epel_repo,
+  Boolean $configure_scl_repo = $::foreman::configure_scl_repo,
+  String $scl_repo_ensure = 'installed',
 ) {
-  $osreleasemajor = regsubst($::operatingsystemrelease, '^(\d+)\..*$', '\1')
-
   if $configure_epel_repo {
-    $epel_gpgkey = $osreleasemajor ? {
-      '7'     => 'https://fedoraproject.org/static/352C64E5.txt',
-      default => 'https://fedoraproject.org/static/0608B895.txt',
-    }
     yumrepo { 'epel':
-      descr      => "Extra Packages for Enterprise Linux ${osreleasemajor} - \$basearch",
-      mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-${osreleasemajor}&arch=\$basearch",
-      baseurl    => "http://download.fedoraproject.org/pub/epel/${osreleasemajor}/\$basearch",
+      descr      => "Extra Packages for Enterprise Linux ${::operatingsystemmajrelease} - \$basearch",
+      mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-${::operatingsystemmajrelease}&arch=\$basearch",
+      baseurl    => "http://download.fedoraproject.org/pub/epel/${::operatingsystemmajrelease}/\$basearch",
       enabled    => 1,
       gpgcheck   => 1,
-      gpgkey     => $epel_gpgkey,
+      gpgkey     => 'https://fedoraproject.org/static/352C64E5.txt',
     }
   }
 
   if $configure_scl_repo {
     package {'foreman-release-scl':
-      ensure => installed,
+      ensure => $scl_repo_ensure,
     }
   }
 }
