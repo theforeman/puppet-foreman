20cad259bda0a3c2dd86b9289cc12809558787ed
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-11-14 13:36:50 +0100

    Make the apt repository parameters

--- manifests/repos/apt.pp
+++ manifests/repos/apt.pp
@@ -1,23 +1,27 @@
 # Install an apt repo
 define foreman::repos::apt (
   Variant[Enum['nightly'], Pattern['^\d+\.\d+$']] $repo,
+  Variant[String, Hash] $key = 'AE0AF310E2EA96B6B6F4BD726F8600B9563278F6',
+  Stdlib::Httpurl $location = 'https://deb.theforeman.org/',
 ) {
   include ::apt
 
-  Apt::Source {
-    location => 'https://deb.theforeman.org/',
-    key      => 'AE0AF310E2EA96B6B6F4BD726F8600B9563278F6',
+  ::apt::source { $name:
+    repos    => $repo,
+    location => $location,
+    key      => $key,
     include  => {
       src => false,
     },
   }
 
-  ::apt::source { $name:
-    repos => $repo,
-  }
-
   ::apt::source { "${name}-plugins":
-    release => 'plugins',
-    repos   => $repo,
+    release  => 'plugins',
+    repos    => $repo,
+    location => $location,
+    key      => $key,
+    include  => {
+      src => false,
+    },
   }
 }
