ced8d72e98c067ddb263a2f76f9dc98e85c0035f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-11-14 11:45:38 +0100

    Test hammer plugin for foreman_ansible

---
+++ spec/classes/foreman_cli_ansible_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'foreman::cli::ansible' do
+  let(:pre_condition) { 'include foreman::cli' }
+
+  context 'on redhat-7-x86_64' do
+    let(:facts) { on_supported_os['redhat-7-x86_64'] }
+    it { should contain_package('tfm-rubygem-hammer_cli_foreman_ansible') }
+  end
+end
