5ca786ac3fb848a8a552fa5abf0ad9b9a98a8b72
Michael Moll <kvedulv@kvedulv.de>
2018-12-25 02:01:51 +0100

    stop apache before tests and add curl check

--- spec/acceptance/foreman_statsd_spec.rb
+++ spec/acceptance/foreman_statsd_spec.rb
@@ -5,10 +5,12 @@ describe 'Scenario: install foreman with statsd' do
     case fact('osfamily')
     when 'RedHat'
       on default, 'yum -y remove foreman* tfm-* && rm -rf /etc/yum.repos.d/foreman*.repo'
+      on default, 'service httpd stop', { :acceptable_exit_codes => [0, 5] }
     when 'Debian'
       on default, 'apt-get purge -y foreman*', { :acceptable_exit_codes => [0, 100] }
       on default, 'apt-get purge -y ruby-hammer-cli-*', { :acceptable_exit_codes => [0, 100] }
       on default, 'rm -rf /etc/apt/sources.list.d/foreman*'
+      on default, 'service apache2 stop', { :acceptable_exit_codes => [0, 5] }
     end
   end
 
@@ -80,6 +82,10 @@ describe 'Scenario: install foreman with statsd' do
 
   describe port(443) do
     it { is_expected.to be_listening }
+  end
+
+  describe command("curl -s --cacert /etc/foreman/certificate.pem https://#{host_inventory['fqdn']} -w '\%{redirect_url}' -o /dev/null") do
+    its(:stdout) { is_expected.to eq("https://#{host_inventory['fqdn']}/users/login") }
   end
 
   # TODO: actually verify statsd functionality
