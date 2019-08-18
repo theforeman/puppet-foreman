1d64237f1223c0b0e0b8a11c5442382b3713b8d2
Ivan Nečas <inecas@redhat.com>
2019-08-18 21:38:29 +0200

    Add rex cockpit integration support
    This adds --foreman-plugin-remote-execution-cockpit boolean option to
    add support for cockpit installation.
    
    It enables foreman-cockpit.service (comming from
    rubygem-foreman_remote_execution-cockpit package), that provides the
    backend service for the cockpit tunel, and configures the settings
    and authentication for the whole setup to work.

--- manifests/plugin/remote_execution.pp
+++ manifests/plugin/remote_execution.pp
@@ -1,7 +1,70 @@
+# = Foreman Remote Execution
 # Installs foreman_remote_execution plugin
-class foreman::plugin::remote_execution {
+#
+# === Parameters:
+#
+# $cockpit:: Install cockpit support
+#
+class foreman::plugin::remote_execution (
+  Boolean $cockpit = $::foreman::plugin::remote_execution::params::cockpit,
+) inherits foreman::plugin::remote_execution::params {
   include ::foreman::plugin::tasks
 
   foreman::plugin {'remote_execution':
+  }
+
+  if $cockpit {
+    case $::osfamily {
+      'RedHat': {
+        case $::operatingsystem {
+          'fedora': {
+            $cockpit_package = 'rubygem-foreman_remote_execution-cockpit'
+          }
+          default: {
+            $cockpit_package = 'tfm-rubygem-foreman_remote_execution-cockpit'
+          }
+        }
+      }
+      default: {
+        fail("${::hostname}: foreman_remote_execution cockpit integration does not support osfamily ${::osfamily}")
+      }
+    }
+
+    package { $cockpit_package:
+      ensure => present,
+    }
+    ->
+    service { 'foreman-cockpit':
+      ensure     => running,
+      enable     => true,
+      hasstatus  => true,
+      hasrestart => true,
+    }
+
+    file { '/etc/foreman/cockpit/cockpit.conf':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('foreman/remote_execution_cockpit.conf.erb'),
+      notify  => Service['foreman-cockpit'],
+    }
+
+    file { '/etc/foreman/cockpit/foreman-cockpit-session.yml':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('foreman/remote_execution_cockpit_session.yml.erb'),
+      notify  => Service['foreman-cockpit'],
+    }
+
+    foreman::config::apache::fragment { 'cockpit':
+      ssl_content => template('foreman/cockpit-apache-ssl.conf.erb'),
+    }
+
+    foreman_config_entry { 'remote_execution_cockpit_url':
+      value => "/webcon/=%{host}",
+    }
   }
 }
---
+++ manifests/plugin/remote_execution/params.pp
@@ -0,0 +1,4 @@
+# Data for the remote_execution plugin
+class foreman::plugin::remote_execution::params {
+  $cockpit = false
+}
