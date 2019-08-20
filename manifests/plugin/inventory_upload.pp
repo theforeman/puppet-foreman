24bb5760ad0b131c932bfeea096a49484087b070
Shimon Shtein <sshtein@redhat.com>
2019-08-20 16:34:44 +0300

    Add support for foreman_inventory_upload plugin

---
+++ manifests/plugin/inventory_upload.pp
@@ -0,0 +1,5 @@
+# Installs inventory_upload plugin
+class foreman::plugin::inventory_upload {
+  foreman::plugin {'inventory_upload':
+  }
+}
---
+++ spec/classes/plugin/inventory_upload_spec.rb
@@ -0,0 +1,6 @@
+
+require 'spec_helper'
+
+describe 'foreman::plugin::inventory_upload' do
+  include_examples 'basic foreman plugin tests', 'inventory_upload'
+end
