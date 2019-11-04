cd6d2056c57f6d2ef960f634f13255067e595544
Eric D. Helms <ericdhelms@gmail.com>
2019-11-04 15:04:46 -0500

    Update postgres SCL

--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -1,5 +1,22 @@
 # Set up the foreman database using postgresql
 class foreman::database::postgresql {
+  case $::osfamily {
+    'RedHat': {
+      if $::operatingsystemrelease =~ /^7/ {
+        class { 'postgresql::globals':
+          version => '10',
+          client_package_name => 'rh-postgresql10-postgresql-syspaths',
+          server_package_name => 'rh-postgresql10-postgresql-server-syspaths',
+          contrib_package_name => 'rh-postgresql10-postgresql-contrib-syspaths',
+          service_name => 'postgresql',
+          datadir => '/var/lib/pgsql/data',
+          confdir => '/var/lib/pgsql/data',
+          bindir => '/usr/bin',
+        }
+      }
+    }
+  }
+
   $dbname = $::foreman::db_database ? {
     'UNSET' => 'foreman',
     default => $::foreman::db_database,
