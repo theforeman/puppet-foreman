87cdfc980eea6ec43ba1377aed43b733118071fa
Shimon Shtein <sshtein@redhat.com>
2020-04-02 12:30:19 +0300

    Rename inventory_upload to rh_cloud

---
+++ manifests/plugin/rh_cloud.pp
@@ -0,0 +1,5 @@
+# Installs rh_cloud plugin
+class foreman::plugin::rh_cloud {
+  foreman::plugin {'rh_cloud':
+  }
+}
---
+++ spec/classes/plugin/rh_cloud_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'foreman::plugin::rh_cloud' do
+  include_examples 'basic foreman plugin tests', 'rh_cloud'
+end
