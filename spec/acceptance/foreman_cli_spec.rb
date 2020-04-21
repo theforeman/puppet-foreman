be039d6568963435d921847972c67c757ebba966
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-21 11:28:00 +0200

    Move repository setup to the acceptance helper
    Note that it disables EPEL which we no longer need.

--- spec/acceptance/foreman_cli_spec.rb
+++ spec/acceptance/foreman_cli_spec.rb
@@ -4,23 +4,15 @@ describe 'Scenario: install foreman-cli without foreman' do
   before(:context) do
     case fact('osfamily')
     when 'RedHat'
-      on default, 'yum -y remove foreman* tfm-* && rm -rf /etc/yum.repos.d/foreman*.repo'
+      on default, 'yum -y remove foreman* tfm-*'
     when 'Debian'
       on default, 'apt-get purge -y foreman*', { :acceptable_exit_codes => [0, 100] }
       on default, 'apt-get purge -y ruby-hammer-cli-*', { :acceptable_exit_codes => [0, 100] }
-      on default, 'rm -rf /etc/apt/sources.list.d/foreman*'
     end
   end
 
   let(:pp) do
-    configure = fact('osfamily') == 'RedHat' && fact('operatingsystem') != 'Fedora'
     <<-EOS
-    class { '::foreman::repo':
-      repo                => 'nightly',
-      gpgcheck            => true,
-      configure_epel_repo => #{configure},
-      configure_scl_repo  => #{configure},
-    } ->
     class { '::foreman::cli':
       foreman_url => 'https://foreman.example.com',
       username    => 'admin',
