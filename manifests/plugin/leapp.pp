c6562e137ecdae220e358b8329c9d160132dc571
Leos Stejskal <github@stejskalleos.cz>
2020-04-30 15:57:57 +0200

    Update manifests/plugin/leapp.pp
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- manifests/plugin/leapp.pp
+++ manifests/plugin/leapp.pp
@@ -1,7 +1,7 @@
 # Installs foreman_leapp plugin
 class foreman::plugin::leapp {
-  include ::foreman::plugin::remote_execution
-  include ::foreman::plugin::ansible
+  include foreman::plugin::remote_execution
+  include foreman::plugin::ansible
 
   foreman::plugin {'leapp':
   }
