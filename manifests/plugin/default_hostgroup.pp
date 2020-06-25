e3a7252ea0e86ff069ccc89de3d6a1b0482fdfa7
Anand Patel <apatelKmd@github.user>
2020-06-25 09:56:34 -0400

    Fixes #30078 - add parameter to accept a hostgroup config hash

--- manifests/plugin/default_hostgroup.pp
+++ manifests/plugin/default_hostgroup.pp
@@ -1,4 +1,18 @@
-# Installs foreman_default_hostgroup plugin
-class foreman::plugin::default_hostgroup {
-  foreman::plugin {'default_hostgroup': }
+# @summary This class installs the default_hostgroup plugin and optionally manages the configuration file
+#
+# @param hostgroups An array of hashes of hostgroup names and facts to add to the configuration
+#
+class foreman::plugin::default_hostgroup (
+  Array[Hash[String, Hash]] $hostgroups = [],
+){
+  if empty($hostgroups) {
+    $config = undef
+  } else {
+    $config = template('foreman/default_hostgroup.yaml.erb')
+  }
+
+  foreman::plugin {'default_hostgroup':
+    config      => $config,
+    config_file => "${foreman::plugin_config_dir}/default_hostgroup.yaml",
+  }
 }
--- spec/classes/plugin/default_hostgroup_spec.rb
+++ spec/classes/plugin/default_hostgroup_spec.rb
@@ -2,4 +2,44 @@ require 'spec_helper'
 
 describe 'foreman::plugin::default_hostgroup' do
   include_examples 'basic foreman plugin tests', 'default_hostgroup'
+
+  context 'with user provided config hash' do
+    let(:params) do
+      {
+        :hostgroups => [
+          'Redhat' => {
+            'osfamily'=> 'RedHat',
+            'lsbdistcodename' => 'Santiago',
+          },
+          'Osx/common' => {
+            'osfamily' => 'Darwin',
+          },
+          'Base' => {
+            'hostname' => '.*'
+          }
+        ]
+      }
+    end
+
+hostgroups = <<EOF
+---
+:default_hostgroup:
+  :facts_map:
+    'Redhat':
+      'lsbdistcodename': 'Santiago'
+      'osfamily': 'RedHat'
+    'Osx/common':
+      'osfamily': 'Darwin'
+    'Base':
+      'hostname': '.*'
+EOF
+
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_file('/etc/foreman/plugins/default_hostgroup.yaml').with_content(hostgroups) }
+  end
+
+  context 'no config hash' do
+    it { is_expected.not_to contain_file('/etc/foreman/plugins/default_hostgroup.yaml') }
+  end
 end
+
