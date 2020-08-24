7f172a89648519ba98061ae415d78b469104531c
Han Boetes <h.boetes@axis-simulation.com>
2020-08-24 15:16:25 +0200

    unsplit and rename to realm.rb

--- lib/facter/realm.rb
+++ lib/facter/realm.rb
@@ -1,6 +1,18 @@
-require_relative 'util/sssd'
-if defined? Facter::Util::Sssd
-  # == Fact: foreman_sssd
+require_relative 'util/realm'
+if defined? Facter::Util::Realm
+  # == Fact: foreman_ipa
+  Facter.add(:foreman_ipa, :type => :aggregate) do
+    {
+      :default_realm => 'global/realm',
+      :default_server => 'global/server',
+    }.each do |key, path|
+      chunk(key) do
+        val = Facter::Util::Realm.ipa_value(path)
+        {key => val} if val
+        # == Fact: foreman_sssd
+      end
+    end
+  end
   Facter.add(:foreman_sssd, :type => :aggregate) do
     {
       :services => 'target[.="sssd"]/services',
@@ -10,7 +22,7 @@ if defined? Facter::Util::Sssd
       :user_attributes => 'target[.="ifp"]/user_attributes',
     }.each do |key, path|
       chunk(key) do
-        val = Facter::Util::Sssd.sssd_value(path)
+        val = Facter::Util::Realm.sssd_value(path)
         {key => val} if val
       end
     end
--- lib/facter/util/realm.rb
+++ lib/facter/util/realm.rb
@@ -1,7 +1,7 @@
 begin
   require 'augeas'
 
-  module Facter::Util::Ipa
+  module Facter::Util::Realm
     def self.aug_value(lens, file, path)
       Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD) do |aug|
         aug.transform(:lens => lens, :incl => file)
@@ -14,8 +14,21 @@ begin
     def self.ipa_value(path)
       aug_value('Puppet.lns', '/etc/ipa/default.conf', path)
     end
-  end
 
+    def self.aug_value(lens, file, path)
+      Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD) do |aug|
+        aug.transform(:lens => lens, :incl => file)
+        aug.load
+        aug.set('/augeas/context', "/files#{file}")
+        aug.get(path)
+      end
+    end
+
+    def self.sssd_value(path)
+      val = aug_value('Sssd.lns', '/etc/sssd/sssd.conf', path)
+      val.split(',').map(&:strip) if val
+    end
+  end
 rescue LoadError => e
   Facter.debug("Cannot load Augeas library for custom facts: #{e}")
 end
