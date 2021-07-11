98d3bc6ecf9d17f0cf3c91e1187489b227dea50b
Cocker Koch <github@cocker.cc>
2021-07-11 11:07:38 +0200

    Let Function to_symbolized_yaml handle Datatype Sensitive

--- spec/functions/foreman_to_symbolized_yaml_spec.rb
+++ spec/functions/foreman_to_symbolized_yaml_spec.rb
@@ -8,4 +8,7 @@ describe 'foreman::to_symbolized_yaml' do
   it 'should symbolize keys' do
     is_expected.to run.with_params({'a' => 'b'}).and_return("---\n:a: b\n")
   end
+  it 'should accept Datatype Sensitive' do
+    expect(subject.execute({'a' => sensitive('b')}).unwrap).to eq("---\n:a: b\n")
+  end
 end
