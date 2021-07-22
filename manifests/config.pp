85514dfd818a3891c8b9da2a291fbb1d00b00cd3
chr1s692 <christian-sam@gmx.de>
2021-07-22 14:58:55 +0200

    Handle duplicate file declaration for foreman::app_root
    Currently we're using your module to install and configure Foreman.
    We want to install Foreman in its own filesystem, but the file resource for
    $foreman::app_root makes it hard to implement this in puppet - it will always
    result in duplicate resource.  Furthermore I'm not sure, if this resource is
    necessary or placed in the right class (ordering, first install the package and
    then create the directory?!).
    
    Testing if this directory is already defined gives other the chance to do some
    stuff, e.g. creating a filesystem before installing foreman :)

--- manifests/config.pp
+++ manifests/config.pp
@@ -50,8 +50,10 @@ class foreman::config {
     content  => template('foreman/foreman.service-overrides.erb'),
   }
 
-  file { $foreman::app_root:
-    ensure  => directory,
+  if ! defined(File[$foreman::app_root]) {
+    file { $foreman::app_root:
+      ensure  => directory,
+    }
   }
 
   if $foreman::db_root_cert {
