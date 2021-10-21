36372529a1392c9fc2a08416269834176acb0972
Oleh Fedorenko <ofedoren@redhat.com>
2021-10-21 11:28:43 +0000

    Refs #33760 - Add host_reports plugin

---
+++ manifests/plugin/host_reports.pp
@@ -0,0 +1,8 @@
+# = Foreman Host Reports plugin
+#
+# This class installs host_reports plugin
+#
+class foreman::plugin::host_reports {
+  foreman::plugin {'host_reports':
+  }
+}
---
+++ spec/classes/plugin/host_reports_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'foreman::plugin::host_reports' do
+  include_examples 'basic foreman plugin tests', 'host_reports'
+end
