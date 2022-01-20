theforeman/puppet-foreman

commit 699f944c93b8cf3718b16e6b9de7143c1e55bbe8

Accept Datatype Sensitive for all Secrets
- accept Datatype Sensitive for Puppet-Type foreman_config_entry
- accept Datatype Sensitive for CLI-Password
- accept Datatype Sensitive for initial Admin-Password
- accept Datatype Sensitive for Database-Password
- accept Datatype Sensitive for OAuth-Secrets
- accept Datatype Sensitive for SMTP-Secrets

manifests/cli.pp
@@ -30,7 +30,7 @@ class foreman::cli (
   String $version = $foreman::cli::params::version,
   Boolean $manage_root_config = $foreman::cli::params::manage_root_config,
   Optional[String] $username = $foreman::cli::params::username,
-  Optional[String] $password = $foreman::cli::params::password,
+  Optional[Variant[String, Sensitive[String]]] $password = $foreman::cli::params::password,
   Boolean $use_sessions = $foreman::cli::params::use_sessions,
   Boolean $refresh_cache = $foreman::cli::params::refresh_cache,
   Integer[-1] $request_timeout = $foreman::cli::params::request_timeout,

manifests/init.pp
@@ -215,7 +215,7 @@ class foreman (
   Variant[Undef, Enum['UNSET'], Stdlib::Port] $db_port = 'UNSET',
   Optional[String] $db_database = 'UNSET',
   Optional[String] $db_username = $foreman::params::db_username,
-  Optional[String] $db_password = $foreman::params::db_password,
+  Optional[Variant[String, Sensitive[String]]] $db_password = $foreman::params::db_password,
   Optional[String] $db_sslmode = 'UNSET',
   Optional[String] $db_root_cert = undef,
   Integer[0] $db_pool = $foreman::params::db_pool,
@@ -234,11 +234,11 @@ class foreman (
   Stdlib::Absolutepath $client_ssl_key = $foreman::params::client_ssl_key,
   Boolean $oauth_active = $foreman::params::oauth_active,
   Boolean $oauth_map_users = $foreman::params::oauth_map_users,
-  String $oauth_consumer_key = $foreman::params::oauth_consumer_key,
-  String $oauth_consumer_secret = $foreman::params::oauth_consumer_secret,
+  Variant[String, Sensitive[String]] $oauth_consumer_key = $foreman::params::oauth_consumer_key,
+  Variant[String, Sensitive[String]] $oauth_consumer_secret = $foreman::params::oauth_consumer_secret,
   String $oauth_effective_user = $foreman::params::oauth_effective_user,
   String $initial_admin_username = $foreman::params::initial_admin_username,
-  String $initial_admin_password = $foreman::params::initial_admin_password,
+  Variant[String, Sensitive[String]] $initial_admin_password = $foreman::params::initial_admin_password,
   Optional[String] $initial_admin_first_name = $foreman::params::initial_admin_first_name,
   Optional[String] $initial_admin_last_name = $foreman::params::initial_admin_last_name,
   Optional[String] $initial_admin_email = $foreman::params::initial_admin_email,
@@ -265,7 +265,7 @@ class foreman (
   Optional[Stdlib::Fqdn] $email_smtp_domain = $foreman::params::email_smtp_domain,
   Enum['none', 'plain', 'login', 'cram-md5'] $email_smtp_authentication = $foreman::params::email_smtp_authentication,
   Optional[String] $email_smtp_user_name = $foreman::params::email_smtp_user_name,
-  Optional[String] $email_smtp_password = $foreman::params::email_smtp_password,
+  Optional[Variant[String, Sensitive[String]]] $email_smtp_password = $foreman::params::email_smtp_password,
   Optional[String] $email_reply_address = $foreman::params::email_reply_address,
   Optional[String] $email_subject_prefix = $foreman::params::email_subject_prefix,
   String $telemetry_prefix = $foreman::params::telemetry_prefix,

templates/hammer_root.yml.epp
@@ -1,8 +1,8 @@
 <%- |
   Optional[String] $username,
-  Optional[String] $password,
+  Optional[Variant[String, Sensitive[String]]] $password,
 | -%>
 :foreman:
   # Credentials. You'll be asked for the interactively if you leave them blank here
-  :username: '<%= $username %>'
-  :password: '<%= $password %>'
+  :username:<%= if $username { " '${username}'" } %>
+  :password:<%= if $password { " '${password}'" } %>

lib/puppet/provider/foreman_config_entry/cli.rb
@@ -65,6 +65,8 @@ Puppet::Type.type(:foreman_config_entry).provide(:cli) do
 
   def value=(value)
     return if resource[:dry]
+
+    value = value.unwrap if value.respond_to?(:unwrap)
     run_foreman_config("-k '#{name}' -v '#{value}'", :combine => true, :failonfail => true)
     @property_hash[:value] = value
   end

manifests/database.pp
@@ -14,7 +14,7 @@ class foreman::database(
   if $foreman::db_manage_rake {
     $seed_env = {
       'SEED_ADMIN_USER'       => $foreman::initial_admin_username,
-      'SEED_ADMIN_PASSWORD'   => $foreman::initial_admin_password,
+      'SEED_ADMIN_PASSWORD'   => if $foreman::initial_admin_password =~ Sensitive {$foreman::initial_admin_password.unwrap} else {$foreman::initial_admin_password},
       'SEED_ADMIN_FIRST_NAME' => $foreman::initial_admin_first_name,
       'SEED_ADMIN_LAST_NAME'  => $foreman::initial_admin_last_name,
       'SEED_ADMIN_EMAIL'      => $foreman::initial_admin_email,

manifests/plugin.pp
@@ -27,7 +27,7 @@ define foreman::plugin(
   String[1] $config_file_owner = 'root',
   String[1] $config_file_group = $foreman::group,
   Stdlib::Filemode $config_file_mode = '0640',
-  Optional[String] $config = undef,
+  Optional[Variant[String, Sensitive[String]]] $config = undef,
 ) {
   # Debian gem2deb converts underscores to hyphens
   case $facts['os']['family'] {

manifests/plugin/supervisory_authority.pp
@@ -30,7 +30,7 @@
 #
 class foreman::plugin::supervisory_authority (
   Stdlib::HTTPUrl              $server_url,
-  String                       $secret_token,
+  Variant[String, Sensitive[String]] $secret_token,
   Pattern[/^[a-zA-Z0-9 _-]+$/] $service_name,
   Integer[0,5]                 $log_level             = 1,
   Integer[0]                   $pool_size             = 1,

spec/classes/foreman_spec.rb
@@ -263,6 +263,20 @@ describe 'foreman' do
         end
       end
 
+      describe 'with all parameters and Sensitive for Secrets' do
+        let :params do
+          {
+            db_password: sensitive('secret'),
+            oauth_consumer_key: sensitive('random'),
+            oauth_consumer_secret: sensitive('random'),
+            initial_admin_password: sensitive('secret'),
+            email_smtp_password: sensitive('secret'),
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+      end
+
       context 'with journald logging' do
         let(:params) { super().merge(logging_type: 'journald') }
         it { is_expected.to compile.with_all_deps }

commit 41b1b45e355aa43ff662c4ae4ce0477a78580b1a

Pass potentially Sensitive params as Sensitive
In 699f944c93b8cf3718b16e6b9de7143c1e55bbe8 the parameters started to
accept Sensitive but it didn't default to Sensitive. They also weren't
converting data coming from Hiera. This adds a data-in-modules setup and
sets lookup_options for those. This means Kafo (which heavily relies on
Hiera) will pass sensitive values.

The commit also missed settings.yml.erb and database.yml.erb which now
also unwraps if needed. Ideally this would be converted to EPP instead
but this fixes it in the short term. However, hammer_root.yml.epp
somehow doesn't properly render the sensitive data. That's why it's
unwrapper for now, just to make it work. A test case is added to prevent
future regressions.

It also changes the data type to accept Sensitive[Undef] which is needed
if Hiera unconditionally converts the value to Sensitive.

Fixes: 699f944c93b8cf3718b16e6b9de7143c1e55bbe8

data/common.yaml
@@ -1,0 +1,9 @@
+lookup_options:
+  '^foreman::(db|email_smtp|initial_admin)_password$':
+    convert_to: "Sensitive"
+  '^foreman::oauth_consumer_(key|secret)$':
+    convert_to: "Sensitive"
+  foreman::cli::password:
+    convert_to: "Sensitive"
+  foreman::plugin::supervisory_authority::secret_token:
+    convert_to: "Sensitive"

hiera.yaml
@@ -1,0 +1,10 @@
+---
+version: 5
+
+defaults:  # Used for any hierarchy level that omits these keys.
+  datadir: data         # This path is relative to hiera.yaml's directory.
+  data_hash: yaml_data  # Use the built-in YAML backend.
+
+hierarchy:
+  - name: "common"
+    path: "common.yaml"

manifests/cli.pp
@@ -30,7 +30,7 @@ class foreman::cli (
   String $version = $foreman::cli::params::version,
   Boolean $manage_root_config = $foreman::cli::params::manage_root_config,
   Optional[String] $username = $foreman::cli::params::username,
-  Optional[Variant[String, Sensitive[String]]] $password = $foreman::cli::params::password,
+  Variant[Optional[String], Sensitive[Optional[String]]] $password = $foreman::cli::params::password,
   Boolean $use_sessions = $foreman::cli::params::use_sessions,
   Boolean $refresh_cache = $foreman::cli::params::refresh_cache,
   Integer[-1] $request_timeout = $foreman::cli::params::request_timeout,
@@ -93,7 +93,7 @@ class foreman::cli (
         'foreman/hammer_root.yml.epp',
         {
           username => $username_real,
-          password => $password_real,
+          password => if $password_real =~ Sensitive { $password_real.unwrap } else { $password_real },
         }
       ),
     }

manifests/init.pp
@@ -215,7 +215,7 @@ class foreman (
   Variant[Undef, Enum['UNSET'], Stdlib::Port] $db_port = 'UNSET',
   Optional[String] $db_database = 'UNSET',
   Optional[String] $db_username = $foreman::params::db_username,
-  Optional[Variant[String, Sensitive[String]]] $db_password = $foreman::params::db_password,
+  Variant[Optional[String], Sensitive[Optional[String]]] $db_password = $foreman::params::db_password,
   Optional[String] $db_sslmode = 'UNSET',
   Optional[String] $db_root_cert = undef,
   Integer[0] $db_pool = $foreman::params::db_pool,
@@ -265,7 +265,7 @@ class foreman (
   Optional[Stdlib::Fqdn] $email_smtp_domain = $foreman::params::email_smtp_domain,
   Enum['none', 'plain', 'login', 'cram-md5'] $email_smtp_authentication = $foreman::params::email_smtp_authentication,
   Optional[String] $email_smtp_user_name = $foreman::params::email_smtp_user_name,
-  Optional[Variant[String, Sensitive[String]]] $email_smtp_password = $foreman::params::email_smtp_password,
+  Variant[Optional[String], Sensitive[Optional[String]]] $email_smtp_password = $foreman::params::email_smtp_password,
   Optional[String] $email_reply_address = $foreman::params::email_reply_address,
   Optional[String] $email_subject_prefix = $foreman::params::email_subject_prefix,
   String $telemetry_prefix = $foreman::params::telemetry_prefix,

manifests/params.pp
@@ -32,7 +32,7 @@ class foreman::params inherits foreman::globals {
   $db_username = 'foreman'
   # Generate and cache the password on the master once
   # In multi-puppetmaster setups, the user should specify their own
-  $db_password = extlib::cache_data('foreman_cache_data', 'db_password', extlib::random_password(32))
+  $db_password = Sensitive(extlib::cache_data('foreman_cache_data', 'db_password', extlib::random_password(32)))
   # Default database connection pool
   $db_pool = 5
   # if enabled, will run rake jobs, which depend on the database
@@ -147,13 +147,13 @@ class foreman::params inherits foreman::globals {
   # We need the REST API interface with OAuth for some REST Puppet providers
   $oauth_active = true
   $oauth_map_users = false
-  $oauth_consumer_key = extlib::cache_data('foreman_cache_data', 'oauth_consumer_key', extlib::random_password(32))
-  $oauth_consumer_secret = extlib::cache_data('foreman_cache_data', 'oauth_consumer_secret', extlib::random_password(32))
+  $oauth_consumer_key = Sensitive(extlib::cache_data('foreman_cache_data', 'oauth_consumer_key', extlib::random_password(32)))
+  $oauth_consumer_secret = Sensitive(extlib::cache_data('foreman_cache_data', 'oauth_consumer_secret', extlib::random_password(32)))
   $oauth_effective_user = 'admin'
 
   # Initial admin account details
   $initial_admin_username = 'admin'
-  $initial_admin_password = extlib::cache_data('foreman_cache_data', 'admin_password', extlib::random_password(16))
+  $initial_admin_password = Sensitive(extlib::cache_data('foreman_cache_data', 'admin_password', extlib::random_password(16)))
   $initial_admin_first_name = undef
   $initial_admin_last_name = undef
   $initial_admin_email = undef

templates/database.yml.erb
@@ -28,6 +28,6 @@
   username: <%= username %>
 <% end -%>
 <% unless (password = scope.lookupvar("::foreman::db_password")) == 'UNSET' -%>
-  password: "<%= password %>"
+  password: "<%= password.respond_to?(:unwrap) ? password.unwrap : password %>"
 <% end -%>
   pool: <%= @db_pool %>

templates/hammer_root.yml.epp
@@ -1,6 +1,6 @@
 <%- |
   Optional[String] $username,
-  Optional[Variant[String, Sensitive[String]]] $password,
+  Variant[Optional[String], Sensitive[Optional[String]]] $password,
 | -%>
 :foreman:
   # Credentials. You'll be asked for the interactively if you leave them blank here

templates/settings.yaml.erb
@@ -12,8 +12,10 @@
 # The following values are used for providing default settings during db migrate
 :oauth_active: <%= scope.lookupvar("foreman::oauth_active") %>
 :oauth_map_users: <%= scope.lookupvar("foreman::oauth_map_users") %>
-:oauth_consumer_key: <%= scope.lookupvar("foreman::oauth_consumer_key") %>
-:oauth_consumer_secret: <%= scope.lookupvar("foreman::oauth_consumer_secret") %>
+<% oauth_key = scope.lookupvar("foreman::oauth_consumer_key") -%>
+:oauth_consumer_key: <%= oauth_key.respond_to?(:unwrap) ? oauth_key.unwrap : oauth_key %>
+<% oauth_secret = scope.lookupvar("foreman::oauth_consumer_secret") -%>
+:oauth_consumer_secret: <%= oauth_secret.respond_to?(:unwrap) ? oauth_secret.unwrap : oauth_secret %>
 
 # Websockets
 :websockets_encrypt: <%= scope.lookupvar("foreman::websockets_encrypt") %>

spec/classes/foreman_cli_spec.rb
@@ -58,6 +58,15 @@ describe 'foreman::cli' do
                 CONFIG
               )
           end
+
+          describe 'using Sensitive' do
+          let(:params) { super().merge(password: sensitive('secret')) }
+
+            it 'should contain settings' do
+              is_expected.to contain_file('/root/.hammer/cli.modules.d/foreman.yml')
+                .with_content(/:password: 'secret'/)
+            end
+          end
         end
 
         describe 'with manage_root_config=false' do