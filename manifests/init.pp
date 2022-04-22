1d28765cf368ff311fce2b227030a436bc72077f
Eric D. Helms <ericdhelms@gmail.com>
2022-04-22 10:58:56 -0400

    Fixes #34640 - Drop apipie:cache:index
    Apipie cache has been explicitly disabled in Foreman so don't waste
    time generating an index.

--- manifests/init.pp
+++ manifests/init.pp
@@ -300,10 +300,6 @@ class foreman (
     $db_sslmode_real = $db_sslmode
   }
 
-  foreman::rake { 'apipie:cache:index':
-    timeout => 0,
-  }
-
   foreman::rake { 'apipie_dsl:cache':
     timeout => 0,
   }
