cc71f4cde6310fd1c5b4443822be567447f8a04f
Eric D. Helms <ericdhelms@gmail.com>
2022-04-22 10:58:56 -0400

    Fixes #34640 - Drop apipie:cache:index
    Apipie cache has been explicitly disabled in Foreman so don't waste
    time generating an index.

--- manifests/database.pp
+++ manifests/database.pp
@@ -27,7 +27,7 @@ class foreman::database(
     foreman::rake { 'db:migrate':
       timeout => $timeout,
       unless  => '/usr/sbin/foreman-rake db:abort_if_pending_migrations',
-      notify  => Foreman::Rake['apipie:cache:index', 'apipie_dsl:cache', 'db:seed'],
+      notify  => Foreman::Rake['apipie_dsl:cache', 'db:seed'],
     }
     ~> foreman_config_entry { 'db_pending_seed':
       value => false,
@@ -35,7 +35,7 @@ class foreman::database(
     }
     ~> foreman::rake { 'db:seed':
       environment => delete_undef_values($seed_env),
-      notify      => Foreman::Rake['apipie:cache:index', 'apipie_dsl:cache'],
+      notify      => Foreman::Rake['apipie_dsl:cache'],
     }
   }
 }
