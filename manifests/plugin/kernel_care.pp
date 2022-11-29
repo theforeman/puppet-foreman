a57aa4b4e2c1ece7713db81bacceaecb51b49156
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-11-29 16:23:43 +0100

    Refs #35800 - Add foreman_kernel_care plugin support

---
+++ manifests/plugin/kernel_care.pp
@@ -0,0 +1,5 @@
+# Installs foreman_kernel_care plugin
+class foreman::plugin::kernel_care {
+  foreman::plugin { 'kernel_care':
+  }
+}
---
+++ spec/classes/plugin/kernel_care_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'foreman::plugin::kernel_care' do
+  include_examples 'basic foreman plugin tests', 'kernel_care'
+end
