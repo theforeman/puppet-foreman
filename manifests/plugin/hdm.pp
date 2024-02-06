96a8323b200f47f51f328308bc4c95312e07e5b7
Martin Alfke <ma@betadots.de>
2024-02-06 14:42:38 +0100

    Add hiera data manager (HDM) plugin

---
+++ manifests/plugin/hdm.pp
@@ -0,0 +1,6 @@
+# @summary This class installs the Hiera Data Manager (HDM) plugin
+#
+class foreman::plugin::hdm {
+  foreman::plugin { 'hdm':
+  }
+}
---
+++ spec/classes/plugin/hdm_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe 'foreman::plugin::hdm' do
+  include_examples 'basic foreman plugin tests', 'hdm'
+end
+
