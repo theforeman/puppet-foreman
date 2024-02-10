a8cfa719abffa554938543734516f1a9f29ff4a7
Martin Alfke <tuxmea@users.noreply.github.com>
2024-02-10 16:08:53 +0100

    Update manifests/plugin/hdm.pp
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- manifests/plugin/hdm.pp
+++ manifests/plugin/hdm.pp
@@ -1,4 +1,4 @@
-# @summary This class installs the Hiera Data Manager (HDM) plugin
+# @summary Install the Hiera Data Manager (HDM) plugin
 #
 class foreman::plugin::hdm {
   foreman::plugin { 'hdm':
