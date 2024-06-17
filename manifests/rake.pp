22d735238b18930e27964ebcd616913fd77d791e
Evgeni Golov <evgeni@golov.de>
2024-06-17 09:55:01 +0200

    always log foreman-rake output
    this is useful when debugging which migrations ran and how long they
    took

--- manifests/rake.pp
+++ manifests/rake.pp
@@ -22,7 +22,7 @@ define foreman::rake (
     command     => "/usr/sbin/foreman-rake ${title}",
     user        => $user,
     environment => sort(join_keys_to_values({ 'HOME' => $app_root } + $environment, '=')),
-    logoutput   => 'on_failure',
+    logoutput   => true,
     refreshonly => $unless =~ Undef,
     timeout     => $timeout,
     unless      => $unless,
