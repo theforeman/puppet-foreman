9d646c664c3a756357c22199cda140a64fe50949
Eric D. Helms <ericdhelms@gmail.com>
2025-01-14 13:28:24 -0500

    Add parameter to enable iop-advisor-engine to rh_cloud plugin
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- manifests/plugin/rh_cloud.pp
+++ manifests/plugin/rh_cloud.pp
@@ -1,5 +1,15 @@
-# Installs rh_cloud plugin
-class foreman::plugin::rh_cloud {
+# @summary Installs rh_cloud plugin
+#
+# @param enable_iop_advisor_engine
+#   Enable iop-advisor-engine integration
+#
+class foreman::plugin::rh_cloud (
+  Boolean $enable_iop_advisor_engine = false,
+) {
   foreman::plugin { 'rh_cloud':
+  }
+
+  class { 'iop_advisor_engine':
+    ensure => bool2str($enable_iop_advisor_engine, 'present', 'absent'),
   }
 }
