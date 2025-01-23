2325c9e05cb23c491396ba0fcb89c0aed733491e
Evgeni Golov <evgeni@golov.de>
2025-01-23 09:26:57 +0100

    drop db between test runs, otherwise migrations might not always pass

--- spec/support/acceptance/purge.rb
+++ spec/support/acceptance/purge.rb
@@ -9,4 +9,5 @@ def purge_foreman
 
   apache_service_name = ['debian', 'ubuntu'].include?(os[:family]) ? 'apache2' : 'httpd'
   on default, "systemctl stop #{apache_service_name}", { :acceptable_exit_codes => [0, 5] }
+  on default, 'runuser - postgres -c "dropdb foreman"', { :acceptable_exit_codes => [0, 1, 127] }
 end
