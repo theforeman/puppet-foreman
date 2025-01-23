02f7e5e86441618e9e339b62e78a814449b3a3dc
Evgeni Golov <evgeni@golov.de>
2025-01-23 10:37:48 +0100

    move repo definition to setup_acceptance_node

--- spec/acceptance/foreman_rh_cloud_spec.rb
+++ spec/acceptance/foreman_rh_cloud_spec.rb
@@ -6,15 +6,6 @@ describe 'Scenario: install foreman with rh_cloud', if: os[:family] == 'redhat' 
   it_behaves_like 'an idempotent resource' do
     let(:manifest) do
       <<-PUPPET
-      yumrepo { 'katello':
-        baseurl  => "http://yum.theforeman.org/katello/nightly/katello/el${facts['os']['release']['major']}/x86_64/",
-        gpgcheck => 0,
-      }
-      yumrepo { 'candlepin':
-        baseurl  => "https://yum.theforeman.org/candlepin/4.4/el${facts['os']['release']['major']}/x86_64/",
-        gpgcheck => 0,
-      }
-
       include foreman
       include foreman::plugin::rh_cloud
       PUPPET
