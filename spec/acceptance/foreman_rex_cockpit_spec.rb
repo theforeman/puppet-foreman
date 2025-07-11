2fa8c5d8ee38a51fc2de21cdc044b5343c39ff44
Eric D. Helms <ericdhelms@gmail.com>
2025-07-11 16:44:59 -0400

    Restart services when enabling cockpit
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- spec/acceptance/foreman_rex_cockpit_spec.rb
+++ spec/acceptance/foreman_rex_cockpit_spec.rb
@@ -17,4 +17,24 @@ describe 'Scenario: install foreman with rex cockpit', if: os[:family] == 'redha
   describe port(19090) do
     it { is_expected.to be_listening.on('127.0.0.1').with('tcp') }
   end
+
+  describe service('foreman-cockpit') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+
+  describe service('httpd') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+
+  describe file('/etc/httpd/conf.d/05-foreman-ssl.d/cockpit.conf') do
+    it { is_expected.to be_file }
+    its(:content) { should match(%r{<Location /webcon>}) }
+    its(:content) { should match(%r{ProxyPass http://127\.0\.0\.1:19090/webcon}) }
+  end
+
+  describe command('curl -k -s -o /dev/null -w "%{http_code}" https://localhost/webcon/') do
+    its(:stdout) { should match(/^(200|302|401)$/) }
+  end
 end
