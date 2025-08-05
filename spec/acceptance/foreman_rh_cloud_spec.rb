de4008f191c093a235a1987143edc127be3b6337
Eric D. Helms <ericdhelms@gmail.com>
2025-08-05 14:12:37 -0400

    Remove puppet-iop_advisor_engine from rh_cloud
    This reverts commit b66e95a87685beb39bd387b24f1f9a72e434757f.
    
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- spec/acceptance/foreman_rh_cloud_spec.rb
+++ spec/acceptance/foreman_rh_cloud_spec.rb
@@ -6,16 +6,6 @@ describe 'Scenario: install foreman with rh_cloud', if: os[:family] == 'redhat' 
   it_behaves_like 'an idempotent resource' do
     let(:manifest) do
       <<-PUPPET
-      # iop_advisor_engine requires foreman-proxy certs to talk back to Foreman
-      # TODO: redesign the deployment in a way that it better aligns with our architecture
-      file { '/etc/foreman-proxy':
-        ensure => directory,
-      }
-
-      group { 'foreman-proxy':
-        ensure => present,
-      }
-
       include foreman
       include foreman::plugin::rh_cloud
       PUPPET
