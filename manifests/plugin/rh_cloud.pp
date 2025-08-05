86134b965b9aa78b6047a4a7d57aa4031265debc
Eric D. Helms <ericdhelms@gmail.com>
2025-08-05 14:12:37 -0400

    Remove puppet-iop_advisor_engine from rh_cloud
    This reverts commit b66e95a87685beb39bd387b24f1f9a72e434757f.
    
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- manifests/plugin/rh_cloud.pp
+++ manifests/plugin/rh_cloud.pp
@@ -1,17 +1,5 @@
 # @summary Installs rh_cloud plugin
-#
-# @param enable_iop_advisor_engine
-#   Enable iop-advisor-engine integration
-#
-class foreman::plugin::rh_cloud (
-  Boolean $enable_iop_advisor_engine = false,
-) {
+class foreman::plugin::rh_cloud {
   foreman::plugin { 'rh_cloud':
-    config => epp('foreman/rh_cloud.yaml.epp', { 'enable_iop_advisor_engine' => $enable_iop_advisor_engine }),
-  }
-
-  class { 'iop_advisor_engine':
-    ensure      => bool2str($enable_iop_advisor_engine, 'present', 'absent'),
-    foreman_url => $foreman::foreman_url,
   }
 }
