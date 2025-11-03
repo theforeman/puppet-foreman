94e861d99eecec50fca917d2c462d4b6ba35aa36
Nadja Heitmann <nadjah@atix.de>
2025-11-03 14:16:05 +0100

    Remove ovirt_provision installer files

--- manifests/plugin/ovirt_provision.pp
+++
@@ -1,6 +0,0 @@
-# @summary install the ovirt_provision plugin
-class foreman::plugin::ovirt_provision {
-  foreman::plugin { 'ovirt_provision':
-    package => $foreman::params::plugin_prefix.regsubst(/foreman[_-]/, 'ovirt_provision_plugin'),
-  }
-}
