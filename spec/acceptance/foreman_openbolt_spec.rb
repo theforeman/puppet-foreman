913539dcfb1a3a13dbbb6efef4ee6c4db048ade6
Tim Meusel <tim@bastelfreak.de>
2025-11-26 16:08:15 +0100

    Add openbolt plugin support
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

---
+++ spec/acceptance/foreman_openbolt_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper_acceptance'
+
+describe 'Scenario: install foreman with openbolt' do
+  before(:context) { purge_foreman }
+
+  it_behaves_like 'an idempotent resource' do
+    let(:manifest) do
+      <<-PUPPET
+      include foreman
+      include foreman::plugin::openbolt
+      PUPPET
+    end
+  end
+
+  it_behaves_like 'the foreman application'
+
+  describe curl_command("https://#{host_inventory['fqdn']}/api/plugins", cacert: '/etc/foreman-certs/certificate.pem', user: 'admin:changeme') do
+    its(:stdout) { should include('foreman_openbolt') }
+    its(:exit_status) { should eq 0 }
+  end
+end
