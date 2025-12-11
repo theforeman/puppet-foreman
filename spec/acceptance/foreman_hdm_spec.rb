3f8d26d12eaa4cacc1237d53c422254054b48bce
Tim Meusel <tim@bastelfreak.de>
2025-12-11 15:36:11 +0100

    Add acceptance test for HDM
    This is a test for our HDM plugin:
    
    https://github.com/betadots/foreman_hdm

---
+++ spec/acceptance/foreman_hdm_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper_acceptance'
+
+describe 'Scenario: install foreman with hdm' do
+  before(:context) { purge_foreman }
+
+  it_behaves_like 'an idempotent resource' do
+    let(:manifest) do
+      <<-PUPPET
+      include foreman
+      include foreman::plugin::hdm
+      PUPPET
+    end
+  end
+
+  it_behaves_like 'the foreman application'
+    describe curl_command("https://admin:changeme@#{host_inventory['fqdn']}/api/plugins", cacert: '/etc/foreman-certs/certificate.pem') do
+    its(:stdout) { should include('foreman_hdm') }
+    its(:exit_status) { should eq 0 }
+  end
+end
